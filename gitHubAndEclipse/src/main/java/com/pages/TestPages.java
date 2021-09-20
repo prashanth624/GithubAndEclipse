11a7566630e0a955024b2506efa04a530ce9902f
prashanth624 <aprashanth246@gmail.com>
2021-09-20 23:46:58 +0530

    First commit

---
+++ gitHubAndEclipse/src/main/java/com/pages/TestPages.java
@@ -0,0 +1,9 @@
+package com.pages;
+
+public class TestPages {
+
+	public void loginPage() {
+		System.out.println("This is a test login page");
+	}
+	
+}
---
+++ gitHubAndEclipse/src/test/java/com/testcases/TestClass.java
@@ -0,0 +1,9 @@
+package com.testcases;
+
+public class TestClass {
+	
+	public void loginTestCase() {
+		System.out.println("This is a test login testcases");
+	}
+
+}
